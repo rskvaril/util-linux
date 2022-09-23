a88074859763f7e37647b42a82bea70fd4e8043f
Radka Brychtova <rskvaril@redhat.com>
2022-09-23 10:54:30 +0200

    add basic fedora setup

---
+++ .github/workflows/cibuild-setup-fedora.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+     
+set -ex
+
+# Xenial uses btrfs-tools, but since Focal it's btrfs-progs
+#
+PACKAGES=(
+	bc
+	btrfs-progs
+	dnsutils
+	libcap-ng-dev
+	libncursesw5-dev
+	libpam-dev
+	libudev-dev
+	gtk-doc-tools
+	mdadm
+	ntp
+	socat
+	asciidoctor
+	meson
+	lcov
+)
+
+COMPILER="${COMPILER:?}"
+PACKAGES+=("$COMPILER")
+
+
+dnf -y update
+dnf -y build-dep util-linux 
+dnf -y install "${PACKAGES[@]}"
+
